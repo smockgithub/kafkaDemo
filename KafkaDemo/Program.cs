f9c227296c262e126c36e1189fe865a6512a496c
smock <smock130@msn.com>
2018-11-06 14:59:14 +0800

    ~

--- KafkaDemo/Program.cs
+++ KafkaDemo/Program.cs
@@ -6,14 +6,29 @@ namespace KafkaDemo_Producer
 {
     class Program
     {
-        static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+            var config = new ProducerConfig { BootstrapServers = "119.23.104.248:9092" };
+
+            Action<DeliveryReportResult<Null, string>> handler = r =>
+            Console.WriteLine(!r.Error.IsError
+                ? $"Delivered message to {r.TopicPartitionOffset}"
+                : $"Delivery Error: {r.Error.Reason}");
+
+            using (var p = new Producer<Null, string>(config))
+            {
+                for (int i = 0; i < 100; ++i)
+                {
+                    p.BeginProduce("my-topic", new Message<Null, string> { Value = i.ToString() }, handler);
+                }
+
+                // wait for up to 10 seconds for any inflight messages to be delivered.
+                p.Flush(TimeSpan.FromSeconds(10));
+            }
 
 
 
-
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Down!");
 
             Console.ReadKey();
         }
