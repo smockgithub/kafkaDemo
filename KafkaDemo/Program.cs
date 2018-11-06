16641e7ac0b10a047174b8ab3b2bb765823dba54
smock <smock130@msn.com>
2018-11-06 14:48:18 +0800

    ~

--- KafkaDemo/Program.cs
+++ KafkaDemo/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Confluent.Kafka;
+using System;
+using System.Threading.Tasks;
 
-namespace KafkaDemo_Product
+namespace KafkaDemo_Producer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+
+
+
             Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
         }
     }
 }
